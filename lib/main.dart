9d3b7a2b663edd68fcbea408096dda6a739d80ff
范国徽 Jack <jackfan1@yonghui.com>
2022-08-07 00:12:47 +0800

    feat： 提交

--- lib/main.dart
+++ lib/main.dart
@@ -33,23 +33,20 @@ Widget _widgetForRoute(String url) {
     case 'route1':
       return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(title: const Text('Flutter Github Actions 发布')),
-          body: ContentWidget(route: route, message: parameters),
-          // Center(
-          //   child:
-          //    Column(
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: <Widget>[
-          //       Text('页面路由名称 $route \n', style: TextStyle(color: Colors.red)),
-          //       //     textDirection: TextDirection.ltr),
-          //       // Text('页面参数 $parameters \n',
-          //       //     style: TextStyle(color: Colors.red),
-          //       //     textDirection: TextDirection.ltr),
-          //       ContentWidget(route: route, message: parameters),
-          //     ],
-          //   ),
-          // )
-        ),
+            appBar: AppBar(title: const Text('Flutter Github Actions 发布')),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('页面路由名称 $route \n',
+                      style: const TextStyle(color: Colors.red)),
+                  Text('页面参数 $parameters \n',
+                      style: TextStyle(color: Colors.red),
+                      textDirection: TextDirection.ltr),
+                  ContentWidget(route: route, message: parameters)
+                ],
+              ),
+            )),
       );
       break;
     default:
